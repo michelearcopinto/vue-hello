be016d8a107bfd2d2bdbe83fa73cf6c3b12fa7e4
michelearcopinto <guestphantom1@gmail.com>
2024-01-25 20:09:42 +0100

    .

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = createApp({
     data() {
         return {
             message: 'vue hello',
-            image: '<img src="https://picsum.photos/200/300">'
+            image: '<img src="https://picsum.photos/200/300" alt="vue-image">'
         }
     }
 }).mount('#app')
